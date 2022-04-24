e56da242735f7d89b478a86537333f4deb639ce4
Artem Baykov <baikoff@bk.ru>
2022-04-24 21:39:08 +0300

    Спустя целый день, меня осенило

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,37 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
 
 @Component
 public class AnimalsCage {
 
-    @Autowired
+
     private Animal animal;
+    private Animal animal2;
+    private Timer time;
+
+    public AnimalsCage(@Qualifier("cat") Animal animal, @Qualifier("dog") Animal animal2, Timer time) {
+        this.animal = animal;
+        this.animal2 = animal2;
+        this.time = time;
+    }
+
+    @Autowired
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(time.getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return time;
     }
 }
