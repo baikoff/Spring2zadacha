6b47209c700fc645a220b9363d75be0a89bd900c
Artem Baykov <baikoff@bk.ru>
2022-05-13 19:07:35 +0300

    Изменил AnimalCage

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -9,20 +9,14 @@ import java.sql.ResultSet;
 @Component
 public class AnimalsCage {
 
-
+    @Autowired
+    @Qualifier("dog")
     private Animal animal;
-    private Animal animal2;
+    @Autowired
     private Timer time;
 
-    public AnimalsCage(@Qualifier("cat") Animal animal, @Qualifier("dog") Animal animal2, Timer time) {
-        this.animal = animal;
-        this.animal2 = animal2;
-        this.time = time;
-    }
 
     @Autowired
-
-
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
